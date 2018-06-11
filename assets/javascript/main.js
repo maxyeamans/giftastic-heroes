649989aa983bd1d80093233853dca24b77034ba8
Max Yeamans <max.yeamans@gmail.com>
2018-06-11 06:02:04 -0700

    Basic layout written

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // List of gif topics
-    var gifTopics = ["Iron Man", "Thor", "Loki", "Captain America", "Hulk", "Guardians of the Galaxy", "Spider-Man", "Ant-Man"];
+    var gifTopics = ["Iron Man", "Thor", "Loki", "Captain America", "Hulk", "Guardians of the Galaxy", "Spider-Man", "Ant-Man", "Scarlet Witch", "Vision", "Doctor Strange"];
 
     // Add buttons to the page
     gifTopics.forEach( function( topic ) {
@@ -8,6 +8,7 @@ $(document).ready(function () {
         var gifButton = $("<button>").text( topic );
         gifButton.attr("type", "button");
         gifButton.addClass("btn btn-primary mx-1");
+        gifButton.val( topic );
         // Add button to header div
         $("header").append(gifButton);
     });
