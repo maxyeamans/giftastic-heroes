8f0d17df362001000ba7d792c905610e5d2b4d50
Max Yeamans <no-reply>
2018-06-11 17:03:35 -0700

    Edits at work

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,29 +4,48 @@ $(document).ready(function () {
 
     AddHeroButtons();
 
-    // Add buttons to the page
+    // Function to render buttons to the page
     function AddHeroButtons() {
         $("header").empty();
-        
+
         gifTopics.forEach(function (topic) {
             // Create button
             var gifButton = $("<button>").text(topic);
             gifButton.attr("type", "button");
-            gifButton.addClass("btn btn-primary m-1");
+            gifButton.addClass("btn btn-primary m-1 gif-button");
             gifButton.val(topic);
             // Add button to header div
             $("header").append(gifButton);
         })
     }
 
+    // Adds a new button to the page on button click
     $("#add-hero").on("click", function (event) {
         event.preventDefault();
 
         var hero = $("#hero-input").val().trim();
-
-        gifTopics.push(hero);
-
-        AddHeroButtons();
-
+        // Check to make sure a value was entered
+        if (hero.length > 0) {
+            gifTopics.push(hero);
+            AddHeroButtons();
+        }
+        else {
+            alert("You gotta type something into the box before submitting!");
+        }
     });
+
+    $(".gif-button").on("click", function( event ) {
+        var gifTopic = this.val();
+        var apiKey = "api_key=HK4AFi8HSTsbwtKlRKpzj9B894V3KCHJ";
+        var queryURL = "https://api.giphy.com/v1/gifs/random?";
+        
+        // AJAX call to Giphy API
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then( function(response){
+
+          })
+    })
+
 })
